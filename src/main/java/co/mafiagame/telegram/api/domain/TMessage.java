d03ebfba3ec0277d367de7c74aca2b45298f54ba
esa <esa.hekmat@gmail.com>
2016-08-13 21:15:14 +0430

    add some more field

--- src/main/java/co/mafiagame/telegram/api/domain/TMessage.java
+++ src/main/java/co/mafiagame/telegram/api/domain/TMessage.java
@@ -28,9 +28,34 @@ import java.util.List;
 public class TMessage {
     @JsonProperty("message_id")
     private Long id;
-    private TChat from;
+    private TUser from;
     private Integer date;
     private TChat chat;
+    @JsonProperty("forward_from")
+    private TUser forwardFrom;
+    @JsonProperty("forward_from_chat")
+    private TChat forwardFromChat;
+    @JsonProperty("forward_date")
+    private Integer forwardDate;
+    @JsonProperty("reply_to_message")
+    private TMessage replyToMessage;
+    @JsonProperty("edit_date")
+    private Integer editDate;
+    private String caption;
+    @JsonProperty("new_chat_member")
+    private TUser newChatMember;
+    @JsonProperty("left_chat_member")
+    private TUser leftChatMember;
+    @JsonProperty("new_chat_title")
+    private String newChatTitle;
+    @JsonProperty("delete_chat_photo")
+    private Boolean deleteChatPhoto;
+    @JsonProperty("group_chat_created")
+    private Boolean groupChatCreated;
+    @JsonProperty("supergroup_chat_created")
+    private Boolean supergroupChatCreated;
+    @JsonProperty("channel_chat_created")
+    private Boolean channelChatCreated;
     private String text;
     private List<TMessageEntity> entities;
 
@@ -42,11 +67,11 @@ public class TMessage {
         this.id = id;
     }
 
-    public TChat getFrom() {
+    public TUser getFrom() {
         return from;
     }
 
-    public void setFrom(TChat from) {
+    public void setFrom(TUser from) {
         this.from = from;
     }
 
@@ -64,6 +89,110 @@ public class TMessage {
 
     public void setChat(TChat chat) {
         this.chat = chat;
+    }
+
+    public TUser getForwardFrom() {
+        return forwardFrom;
+    }
+
+    public void setForwardFrom(TUser forwardFrom) {
+        this.forwardFrom = forwardFrom;
+    }
+
+    public TChat getForwardFromChat() {
+        return forwardFromChat;
+    }
+
+    public void setForwardFromChat(TChat forwardFromChat) {
+        this.forwardFromChat = forwardFromChat;
+    }
+
+    public Integer getForwardDate() {
+        return forwardDate;
+    }
+
+    public void setForwardDate(Integer forwardDate) {
+        this.forwardDate = forwardDate;
+    }
+
+    public TMessage getReplyToMessage() {
+        return replyToMessage;
+    }
+
+    public void setReplyToMessage(TMessage replyToMessage) {
+        this.replyToMessage = replyToMessage;
+    }
+
+    public Integer getEditDate() {
+        return editDate;
+    }
+
+    public void setEditDate(Integer editDate) {
+        this.editDate = editDate;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public TUser getNewChatMember() {
+        return newChatMember;
+    }
+
+    public void setNewChatMember(TUser newChatMember) {
+        this.newChatMember = newChatMember;
+    }
+
+    public TUser getLeftChatMember() {
+        return leftChatMember;
+    }
+
+    public void setLeftChatMember(TUser leftChatMember) {
+        this.leftChatMember = leftChatMember;
+    }
+
+    public String getNewChatTitle() {
+        return newChatTitle;
+    }
+
+    public void setNewChatTitle(String newChatTitle) {
+        this.newChatTitle = newChatTitle;
+    }
+
+    public Boolean getDeleteChatPhoto() {
+        return deleteChatPhoto;
+    }
+
+    public void setDeleteChatPhoto(Boolean deleteChatPhoto) {
+        this.deleteChatPhoto = deleteChatPhoto;
+    }
+
+    public Boolean getGroupChatCreated() {
+        return groupChatCreated;
+    }
+
+    public void setGroupChatCreated(Boolean groupChatCreated) {
+        this.groupChatCreated = groupChatCreated;
+    }
+
+    public Boolean getSupergroupChatCreated() {
+        return supergroupChatCreated;
+    }
+
+    public void setSupergroupChatCreated(Boolean supergroupChatCreated) {
+        this.supergroupChatCreated = supergroupChatCreated;
+    }
+
+    public Boolean getChannelChatCreated() {
+        return channelChatCreated;
+    }
+
+    public void setChannelChatCreated(Boolean channelChatCreated) {
+        this.channelChatCreated = channelChatCreated;
     }
 
     public String getText() {
@@ -89,6 +218,19 @@ public class TMessage {
                 ", from=" + from +
                 ", date=" + date +
                 ", chat=" + chat +
+                ", forwardFrom=" + forwardFrom +
+                ", forwardFromChat=" + forwardFromChat +
+                ", forwardDate=" + forwardDate +
+                ", replyToMessage=" + replyToMessage +
+                ", editDate=" + editDate +
+                ", caption='" + caption + '\'' +
+                ", newChatMember=" + newChatMember +
+                ", leftChatMember=" + leftChatMember +
+                ", newChatTitle='" + newChatTitle + '\'' +
+                ", deleteChatPhoto=" + deleteChatPhoto +
+                ", groupChatCreated=" + groupChatCreated +
+                ", supergroupChatCreated=" + supergroupChatCreated +
+                ", channelChatCreated=" + channelChatCreated +
                 ", text='" + text + '\'' +
                 ", entities=" + entities +
                 '}';
